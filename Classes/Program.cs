810d6fcda554f7ed69086e573ade3f324e1fb828
Mike Cowell <spike@spikeandbetty.com>
2022-10-25 11:03:14 -0400

    init commit

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace Classes
+﻿using System.Runtime.InteropServices;
+
+namespace Classes
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var myCar = new Car();
+            myCar.Make = "Honda";
+            myCar.Model = "Fit";
+            myCar.Year = 2016;
+            var carList = new List<Car>();
+            carList.Add(myCar);
+            Console.WriteLine($"I am currently driving a {myCar.Make} {myCar.Model} that was manufactured in {myCar.Year}");
+            foreach (var thing in carList)
+            {
+                Console.WriteLine(thing.Make);
+                Console.WriteLine(thing.Model);
+                Console.WriteLine(thing.Year);
+            }
+
         }
     }
 }
